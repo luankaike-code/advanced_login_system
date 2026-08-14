2bbfc433e3df07c5e6b46a42d1affc6e05937963
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-14 17:17:04 -0300

    Add instructions on schema.sql: CREATE TABLE users

---
+++ database/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXIST users;
+
+CREATE TABLE users (
+	user_id int PRIMARY KEY AUTOINCREMENT,
+	name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	tel VARCHAR(11) UNIQUE,
+	can_view_user_table BOOL DEFAULT FALSE,
+	can_create_new_user BOOL DEFAULT FALSE
+)
