f5a8b45097d6574520e29fd166db73139673fa24
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-15 14:59:33 -0300

    Create attemps system to create the connection

--- src/bls_db/database_connection_manager.py
+++ src/bls_db/database_connection_manager.py
@@ -1,7 +1,12 @@
-import mysql.connector
+import os, mysql.connector
 from mysql.connector import Error
 from mysql.connector.pooling import PooledMySQLConnection
 from mysql.connector.abstracts import MySQLConnectionAbstract
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MAX_CONNECTION_ATTEMPS = int(os.getenv("MAX_CONNECTION_ATTEMPS"))
 
 class DatabaseConnectionManager:
 	def __init__(self, host: str, port: int, database: str, user: str, password: str) -> None:
@@ -21,17 +26,27 @@ class DatabaseConnectionManager:
 
 	def __create_connection(self) -> (PooledMySQLConnection | MySQLConnectionAbstract | None):
 		connection = None
+		connection_attemps = 0
+		errors = []
 
-		try:
-			connection = mysql.connector.connect(
-				host=self.__host,
-				port=self.__port,
-				database=self.__database,
-				user=self.__user,
-				password=self.__password,
-			)
-		except Error as e:
-			print(f"Error while connecting to MySQL: {e}")
+		def print_all_throw_errors():
+			for i, error in enumerate(errors):
+				print(f"Error #{i} throw while connecting to MySQL: {error}")
+
+		while connection_attemps < MAX_CONNECTION_ATTEMPS:
+			try:
+				connection = mysql.connector.connect(
+					host=self.__host,
+					port=self.__port,
+					database=self.__database,
+					user=self.__user,
+					password=self.__password,
+				)
+			except Error as e:
+				errors.append(e)
+				connection_attemps += 1
+		else:
+			print_all_throw_errors()
 
 		return connection
 
