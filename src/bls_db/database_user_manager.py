1fa358f9aa7ef85709eefcb7dcdf4cf23f023547
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-16 13:51:24 -0300

    Add method 'check_login_information_within_error_handler' to DatabaseUserManager

--- src/bls_db/database_user_manager.py
+++ src/bls_db/database_user_manager.py
@@ -1,15 +1,9 @@
 from mysql.connector import Error
+from mysql.connector.types import RowItemType, RowType
+from typing import Literal, List, Dict, Any
+
 from .database_commands_manager import DatabaseCommandsManager
 
-# CREATE TABLE users (
-# 	user_id INT PRIMARY KEY AUTO_INCREMENT,
-# 	name VARCHAR(255) NOT NULL,
-# 	email VARCHAR(255) UNIQUE NOT NULL,
-# 	password VARCHAR(255) NOT NULL,
-# 	tel VARCHAR(11) UNIQUE,
-# 	can_view_user_table BOOL DEFAULT FALSE,
-# 	can_create_new_user BOOL DEFAULT FALSE
-# )
 
 class DatabaseUserManager(DatabaseCommandsManager):
 	def __init__(self, host: str, port: int, database: str, user: str, password: str):
@@ -20,6 +14,19 @@ class DatabaseUserManager(DatabaseCommandsManager):
 			self.insert_new_user_within_error_handler(name, email, password, tel, can_view_user_table, can_create_new_user)
 		except Error as e:
 			print(f"Error while insert new user: {e}")
+
+	def check_login_information_within_error_handler(self, identifity: str, identifier_type: Literal["name", "email", "tel"], password: str) -> tuple[bool, List[RowType | Dict[str, RowItemType]] | Any]:
+		res = self._execute_select(
+			table="users",
+			fields="*",
+			where_query=f"{identifier_type} = %s AND password = %s",
+			values=[
+				identifity,
+				password
+			]
+		)
+
+		return (len(res) == 1, res)
 
 	def insert_new_user_within_error_handler(self, name: str, email: str, password: str, tel: str, can_view_user_table: bool=False, can_create_new_user: bool=False):
 		self._execute_insert(
