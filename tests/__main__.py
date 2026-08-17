79ab15f5595482714ef16d2ae5db78a394293e12
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-17 16:41:00 -0300

    Populate tests directory

---
+++ tests/__main__.py
@@ -0,0 +1,25 @@
+import os, als_db
+from dotenv import load_dotenv
+
+load_dotenv()
+
+host = os.getenv("HOST")
+port = int(os.getenv("PORT"))
+database = os.getenv("DATABASE")
+user = os.getenv("DATABASE_USER")
+password = os.getenv("PASSWORD")
+
+db = als_db.DatabaseUserManager(
+	host=host,
+	port=port,
+	database=database,
+	user=user,
+	password=password
+)
+
+db.insert_new_user(
+	name="Luka",
+	email="@outllokkk",
+	password="senha",
+	tel="12345677"
+)
