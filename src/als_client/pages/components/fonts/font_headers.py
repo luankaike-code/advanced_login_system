12129adc0a932ac113aff764353658c2d672e85b
Luan Kaike <luankaikemacksantos@gmail.com>
2026-08-18 11:38:42 -0300

    client: Create header fonts

---
+++ src/als_client/pages/components/fonts/font_headers.py
@@ -0,0 +1,13 @@
+import customtkinter as ctk
+
+font_h1 = lambda: ctk.CTkFont(
+	family="Helvetica", 
+	size=24, 
+	weight="bold"
+)
+
+font_h2 = lambda: ctk.CTkFont(
+	family="Helvetica", 
+	size=20, 
+	weight="bold"
+)
