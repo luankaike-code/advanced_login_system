8d79638f3dae41a9639ba42a333e9e822fbba202
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-18 14:27:17 -0300

    Refactore users table to use 'root_access' instead 'can_view' and 'can_create'

--- database/schema.sql
+++ database/schema.sql
@@ -6,6 +6,5 @@ CREATE TABLE users (
 	email VARCHAR(255) UNIQUE NOT NULL,
 	password VARCHAR(255) NOT NULL,
 	tel VARCHAR(11) UNIQUE,
-	can_view_user_table BOOL DEFAULT FALSE,
-	can_create_new_user BOOL DEFAULT FALSE
+	has_sudo_access BOOL DEFAULT FALSE
 )
