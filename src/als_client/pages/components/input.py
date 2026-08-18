2ad15ee6e2101fa5fe82146278a6edcc170ceb1d
Luan Kaike <luankaikemacksantos@gmail.com>
2026-08-18 10:26:58 -0300

    Implement recommend method to get an entry value

--- src/als_client/pages/components/input.py
+++ src/als_client/pages/components/input.py
@@ -5,11 +5,9 @@ class Input(Component):
 	def __init__(self, window: ctk.CTk, label: str, placeholder: str="") -> None:
 		super().__init__()
 
+		self.entry_value = ctk.StringVar()
+
 		self._widgets.update({
 			"label": ctk.CTkLabel(window, text=label),
-			"entry": ctk.CTkEntry(window, placeholder_text=placeholder)
+			"entry": ctk.CTkEntry(window, placeholder_text=placeholder, textvariable=self.entry_value)
 		})
-
-	def get_value(self) -> str:
-		entry: ctk.CTkEntry = self._widgets["entry"]
-		return entry.get()
