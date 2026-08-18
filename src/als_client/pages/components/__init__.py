8b7bdbc7dcda18c53d59e2429fb319c924ea56f5
Luan Kaike <luankaikemacksantos@gmail.com>
2026-08-18 11:54:11 -0300

    client: Add all new components to components export

--- src/als_client/pages/components/__init__.py
+++ src/als_client/pages/components/__init__.py
@@ -1,3 +1,7 @@
 from .input import Input
+from .popup import Popup
+from .paragraph import Paragraph
+from .hs import H1, H2
+from .button import Button
 
-__all__ = ["Input"]
+__all__ = ["Input", "Popup", "Paragraph", "H1", "H2", "Button"]
