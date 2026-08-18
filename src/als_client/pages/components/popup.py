62b8b464006609f76b8912d225db013b07183141
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-18 15:29:34 -0300

    client: Implement Paragraph and H1 component in popup

--- src/als_client/pages/components/popup.py
+++ src/als_client/pages/components/popup.py
@@ -1,4 +1,6 @@
 import customtkinter as CTk
+from .paragraph import Paragraph
+from .hs import H1
 
 class Popup(CTk.CTkToplevel):
 	def __init__(self, *args, title = str, label = str, geometry: str ="200x100", freeze: bool = True, fg_color = None, **kwargs) -> None:
@@ -7,9 +9,9 @@ class Popup(CTk.CTkToplevel):
 		self.title(title)
 		self.geometry(geometry)
 
-		CTk.CTkLabel(self, text=title).pack(pady=10)
+		H1(self, text=title).pack(pady=10)
 
-		CTk.CTkLabel(self, text=label).pack(pady=5)
+		Paragraph(self, text=label).pack(pady=5)
 
 		if freeze:
 			self.grab_set()
