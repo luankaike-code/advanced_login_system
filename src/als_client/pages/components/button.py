fed28e73910902fdcb9412b0914ebd1c28dcae86
Luan Kaike <luankaikemacksantos@gmail.com>
2026-08-18 11:52:37 -0300

    client: Create Button component

---
+++ src/als_client/pages/components/button.py
@@ -0,0 +1,7 @@
+import customtkinter as CTk
+from typing import Callable
+from .fonts import font_normal
+
+class Button(CTk.CTkButton):
+	def __init__(self, master, text: str = "CTkButton", command: Callable | None = None, **kwargs):
+		super().__init__(master, text=text, font=font_normal(), command=command, **kwargs)
