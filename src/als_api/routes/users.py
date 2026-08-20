99bb18317377637921383d46ec6fb414a187ed47
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-20 15:02:00 -0300

    api: Create basic endpoint users/login logic

--- src/als_api/routes/users.py
+++ src/als_api/routes/users.py
@@ -1,18 +1,29 @@
+import os
 from flask import Blueprint, jsonify, request
 
+from .table_managers import TableManagers
+
 users_blueprint = Blueprint("users", __name__, url_prefix="/users")
+
+table_managers = TableManagers()
 
 @users_blueprint.route("/login", methods=["POST"])
 def list_users():
 	data = request.get_json()
-	print(data)
-	identity = data.get("identity")
-	password = data.get("password")
-	print(identity, password)
+
+	identity: str = data.get("identity")
+	type = "email" if "@" in identity else "tel" if identity.isnumeric() else "name"
+	password: str = data.get("password")
+	
 	if not identity or not password:
 		return jsonify({"response": "Informe sua identidade e sua senha"}), 401
+	
+	(result, data) = table_managers.users.check_login_information_without_error_handler(identity, type, password)
 
-	if identity != "root" or password != "root":
-		return jsonify({"response": "Acesso negado"}), 401
+	if not result:
+		return jsonify({"response": "Login unsuccessful"}), 401
 
-	return jsonify({"response": "Acesso permitido"}), 200
+	print(data)
+	token = table_managers.connections.create_new_connection(data.id)
+
+	return jsonify({"response": "Login successfull", "token": token}), 200
