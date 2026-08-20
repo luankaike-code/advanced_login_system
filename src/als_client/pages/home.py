9a6f68c9b031d7b06b515013faaf5e370e0f670d
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-20 15:40:52 -0300

    client: Create basic Home page

---
+++ src/als_client/pages/home.py
@@ -0,0 +1,12 @@
+from .components import H1, Paragraph
+from .page import Page
+
+class Home(Page):
+	def __init__(self, geometry: str = "400x300", title: str = "Login") -> None:
+		super().__init__(geometry, title)
+
+		self._create_widgets()
+	
+	def _create_widgets(self) -> None:
+		H1(self, text="Olá, ...").pack(padx=10, pady=10)
+		Paragraph(self, text="Seja bem-vindo(a)}").pack(padx=10, pady=10)
