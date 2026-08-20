74f3e3240db5ecf97093ebd49b19e51a23ecd9dc
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-20 15:43:22 -0300

    client: Implement all new features in App

--- src/als_client/app.py
+++ src/als_client/app.py
@@ -1,9 +1,12 @@
 import tkinter as tk
-from pages import Login
+from pages import Login, Home
 
 class App():
 	def __init__(self):
 		pass
 
+	def _successful_login(self):
+		Home().mainloop
+	
 	def start(self):
-		Login().mainloop()
+		Login(successful_login_callback=self._successful_login).mainloop()
