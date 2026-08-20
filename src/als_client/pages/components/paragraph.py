6bb904c4ed0d9bab9d99ae9c890500b3e4d01973
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-20 15:21:56 -0300

    client: Implement StringVar in Paragraph logic

--- src/als_client/pages/components/paragraph.py
+++ src/als_client/pages/components/paragraph.py
@@ -3,4 +3,6 @@ from .fonts import font_normal
 
 class Paragraph(CTk.CTkLabel):
 	def __init__(self, master, text = "paragraph", **kwargs):
-		super().__init__(master, text=text, font=font_normal(), **kwargs)
+		self.text = CTk.StringVar(value=text)
+
+		super().__init__(master, textvariable=self.text, font=font_normal(), **kwargs)
