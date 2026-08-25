9ef912a5bb388099e3168831294af6877864895b
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-25 14:28:34 -0300

    client: Refractore Home page to use AbstractHome who your dad

--- src/als_client/pages/home.py
+++ src/als_client/pages/home.py
@@ -1,15 +1,10 @@
-from .components import H1, Paragraph
-from .page import Page
-from als_requests import Request
+from .abstract_home import AbstractHome
 
-class Home(Page):
-	def __init__(self, geometry: str = "400x300", title: str = "Login") -> None:
-		super().__init__(geometry, title)
-
-		self._create_widgets()
-	
-	def _create_widgets(self) -> None:
-		self_data = Request.get_self_infos()
-
-		H1(self, text=f"Olá, {self_data.name}").pack(padx=10, pady=10)
-		Paragraph(self, text="Seja bem-vindo(a)").pack(padx=10, pady=10)
+class Home(AbstractHome):
+	def __init__(self,  geometry: str = "400x300", title: str = "Login") -> None:
+		super().__init__({
+			"Ver minhas informações": lambda: print("Ver minhas informações"),
+			"Acessar tabela de usuários": lambda: print("Acessar tabela de usuários"),
+			"Cadastrar novo usuário": lambda: print("Cadastrar novo usuário"),
+			"Sair": lambda: print("Sair")
+		}, geometry, title)
