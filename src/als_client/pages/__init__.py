1610a7b6d9e2826eb52c814dbfb283450ff6d248
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-28 15:15:54 -0300

    client: Create SelfInformations page

--- src/als_client/pages/__init__.py
+++ src/als_client/pages/__init__.py
@@ -1,4 +1,5 @@
 from .login import Login
 from .home import Home
+from .self_informations import SelfInformations
 
-__all__ = ["Login", "Home"]
+__all__ = ["Login", "Home", "SelfInformations"]
---
+++ src/als_client/pages/self_informations.py
@@ -0,0 +1,27 @@
+from .components import H1, Paragraph, Button, Table
+from .page import Page
+from als_requests import Request
+
+class SelfInformations(Page):
+	def __init__(self, geometry: str = "400x500", title: str = "Informações") -> None:
+		super().__init__(geometry, title)
+		
+		self.infos = Request.get_self_infos()
+
+		self._create_widgets()
+	
+	def _create_widgets(self) -> None:
+		H1(self, text=f"Olá, {self.infos.name}").pack(padx=10, pady=10)
+		Paragraph(self, text="Suas informações abaixo").pack(padx=10, pady=10)
+		table = Table(self)
+
+		table_content = [
+			[Paragraph(table, text="name"), Paragraph(table, text=self.infos.name)],
+			[Paragraph(table, text="email"), Paragraph(table, text=self.infos.email)],
+			[Paragraph(table, text="password"), Paragraph(table, text=self.infos.password)],
+			[Paragraph(table, text="tel"), Paragraph(table, text=self.infos.tel)],
+			[Paragraph(table, text="tem super acesso?"), Paragraph(table, text="sim" if self.infos.has_sudo_access else "não")]
+		]
+
+		table.set_content(table_content)
+		table.pack()
