a50d3f3e632ab8cfbc9bec5277a52531318a50c5
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-28 15:28:42 -0300

    client: Deimplement AbstractHome, now so exist Home

--- src/als_client/pages/home.py
+++ src/als_client/pages/home.py
@@ -1,10 +1,19 @@
-from .abstract_home import AbstractHome
+from typing import Dict, Callable
+from .components import H1, Paragraph, Button
+from .page import Page
+from als_requests import Request
 
-class Home(AbstractHome):
-	def __init__(self,  geometry: str = "400x300", title: str = "Login") -> None:
-		super().__init__({
-			"Ver minhas informações": lambda: print("Ver minhas informações"),
-			"Acessar tabela de usuários": lambda: print("Acessar tabela de usuários"),
-			"Cadastrar novo usuário": lambda: print("Cadastrar novo usuário"),
-			"Sair": self.destroy
-		}, geometry, title)
+class Home(Page):
+	def __init__(self,  buttons: Dict[str, Callable], geometry: str = "400x300", title: str = "Login") -> None:
+		super().__init__(geometry, title)
+
+		self._create_widgets(buttons)
+	
+	def _create_widgets(self,  buttons: Dict[str, Callable]) -> None:
+		self_data = Request.get_self_infos()
+
+		H1(self, text=f"Olá, {self_data.name}").pack(padx=10, pady=10)
+		Paragraph(self, text="Seja bem-vindo(a)").pack(padx=10, pady=10)
+
+		for btn_text, btn_callback in buttons.items():
+			Button(self, text=btn_text, command=btn_callback).pack(padx=10, pady=10)
