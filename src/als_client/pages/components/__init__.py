fd8e104ae0b6872da905c652f2e6bf12024d0049
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-28 15:13:47 -0300

    client: Include component Table at components´s export

--- src/als_client/pages/components/__init__.py
+++ src/als_client/pages/components/__init__.py
@@ -3,5 +3,6 @@ from .popup import Popup
 from .paragraph import Paragraph
 from .hs import H1, H2
 from .button import Button
+from .table import Table
 
-__all__ = ["Input", "Popup", "Paragraph", "H1", "H2", "Button"]
+__all__ = ["Input", "Popup", "Paragraph", "H1", "H2", "Button", "Table"]
