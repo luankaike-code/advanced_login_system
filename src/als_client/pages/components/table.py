b2ba79862fc36faba16700c107ee450e2a184d91
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-28 15:13:04 -0300

    client: Create component Table

---
+++ src/als_client/pages/components/table.py
@@ -0,0 +1,21 @@
+import customtkinter as CTk
+
+class Table(CTk.CTkFrame):
+	def __init__(self, master, width = 200, height = 200):
+		super().__init__(master, width, height)
+
+	def set_content(self, content_data: list[list[CTk.CTkBaseClass]] | None = None) -> None:
+		self.__content_data = content_data
+		self.__update_data()
+
+	def __update_data(self) -> None:
+		if self.__content_data is None:
+			return
+		
+		column_count = len(self.__content_data[0])
+		columns_index = list(i for i in range(column_count))
+		self.grid_columnconfigure(columns_index, weight=1)
+
+		for row, row_data in enumerate(self.__content_data):
+			for column, component in enumerate(row_data):
+				component.grid(row=row, column=column, padx=20, pady=20)
