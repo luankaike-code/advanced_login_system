2a147864ef65eb019b055ed90fb6df3001a64d2b
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-31 14:55:12 -0300

    clent: Refractore App inherit CTk and to use new Page

--- src/als_client/app.py
+++ src/als_client/app.py
@@ -1,30 +1,37 @@
 from pages import Login, Home, SelfInformations
 from pages.page import Page
+import customtkinter as ctk
 from als_requests import Request
 import atexit
 
-class App():
+class App(ctk.CTk):
 	def __init__(self) -> None:
+		super().__init__()
 		atexit.register(Request.logout)
-		self.current_window = None
+		self.current_page = None
 
-	def __update_current_window(self, new_window: Page):
-		if self.current_window:
-			self.current_window.destroy()
+	def __destroy_current_page(self):
+		if self.current_page and self.current_page.winfo_exists():
+			self.current_page.destroy()
 
-		self.current_window = new_window
-		self.current_window.mainloop()
+	def __update_current_page(self, new_page: Page):
+		self.__destroy_current_page()
+
+		self.current_page = new_page
+		self.current_page.pack(fill="both", expand=True)
 
 	def _go_to_home(self) -> None:
-		home_page = Home({
+		home_page = Home(self, {
 			"Ver minhas informações": lambda: print("Ver minhas informações"),
 			"Acessar tabela de usuários": lambda: print("Acessar tabela de usuários"),
 			"Cadastrar novo usuário": lambda: print("Cadastrar novo usuário"),
-			"Sair": lambda: print("Sair")
+			"Sair": self.destroy
 		})
 
-		self.__update_current_window(home_page)
+		self.__update_current_page(home_page)
 	
 	def start(self) -> None:
-		login_page = Login(successful_login_callback=self._go_to_home)
-		self.__update_current_window(login_page)
+		login_page = Login(self, successful_login_callback=self._go_to_home)
+		self.__update_current_page(login_page)
+
+		self.mainloop()
