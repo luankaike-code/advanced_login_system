54a734dd20f23c9ad17cb6a5bf790803b1658c57
luankaike-code <luankaikemacksantos@gmail.com>
2026-08-31 14:15:55 -0300

    client: Refractore how current windows is handled in App

--- src/als_client/app.py
+++ src/als_client/app.py
@@ -1,18 +1,30 @@
 from pages import Login, Home, SelfInformations
+from pages.page import Page
 from als_requests import Request
 import atexit
 
 class App():
 	def __init__(self) -> None:
 		atexit.register(Request.logout)
+		self.current_window = None
 
-	def _successful_login(self) -> None:
-		Home({
+	def __update_current_window(self, new_window: Page):
+		if self.current_window:
+			self.current_window.destroy()
+
+		self.current_window = new_window
+		self.current_window.mainloop()
+
+	def _go_to_home(self) -> None:
+		home_page = Home({
 			"Ver minhas informações": lambda: print("Ver minhas informações"),
 			"Acessar tabela de usuários": lambda: print("Acessar tabela de usuários"),
 			"Cadastrar novo usuário": lambda: print("Cadastrar novo usuário"),
 			"Sair": lambda: print("Sair")
-		}).mainloop()
+		})
+
+		self.__update_current_window(home_page)
 	
 	def start(self) -> None:
-		Login(successful_login_callback=self._successful_login).mainloop()
+		login_page = Login(successful_login_callback=self._go_to_home)
+		self.__update_current_window(login_page)
