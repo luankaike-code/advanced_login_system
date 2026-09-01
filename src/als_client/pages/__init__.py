90189eeed1493bd09a61ca21ea00a602003a9ad1
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-01 15:33:32 -0300

    Create FormUserInformation page

--- src/als_client/pages/__init__.py
+++ src/als_client/pages/__init__.py
@@ -1,5 +1,6 @@
 from .login import Login
 from .home import Home
 from .self_informations import SelfInformations
+from .form_user_informations import FormUserInformations
 
-__all__ = ["Login", "Home", "SelfInformations"]
+__all__ = ["Login", "Home", "SelfInformations", "FormUserInformations"]
---
+++ src/als_client/pages/form_user_informations.py
@@ -0,0 +1,42 @@
+import customtkinter as ctk
+from .page import Page
+from .components import H1, Input, PasswordInput, Button
+
+class FormUserInformations(Page):
+	def __init__(
+			self, 
+			master: ctk.CTk,
+			button_text: str,
+			title: str,
+			display_password_input: bool = False,
+			geometry: str = "400x500"
+		):
+		super().__init__(master, geometry, title)
+
+		self.button_text = button_text
+		self.display_password_input = display_password_input
+
+		self.input_name = Input(self, "Nome", "José Almeida")
+		self.input_email = Input(self, "Email", "JoséAlmeida@gmail.com")
+		self.input_password = PasswordInput(self, "Senha", "******")
+		self.input_tel = Input(self, "Telefone", "(00) 0 0000-0000")
+
+		self._create_widgets()
+
+	def __send_button_callback(self) -> None:
+		print(
+			self.input_name.get_value(),
+			self.input_email.get_value(),
+			self.input_password.get_value(),
+			self.input_tel.get_value(),
+		)
+
+	def _create_widgets(self) -> None:
+		H1(self, self.title).pack(padx=10, pady=10)
+
+		self.input_name.pack_all_widgets(padx=10, pady=5)
+		self.input_email.pack_all_widgets(padx=10, pady=5)
+		self.input_password.pack_all_widgets(padx=10, pady=5)
+		self.input_tel.pack_all_widgets(padx=10, pady=5)
+
+		Button(self, self.button_text, command=self.__send_button_callback).pack(padx=10, pady=5)
