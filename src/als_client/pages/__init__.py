cee1ccbb0396d2688a763d1825fe6f381e072d12
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-02 14:35:14 -0300

    client: Create RegisterNewUser page

--- src/als_client/pages/__init__.py
+++ src/als_client/pages/__init__.py
@@ -2,5 +2,6 @@ from .login import Login
 from .home import Home
 from .self_informations import SelfInformations
 from .form_user_informations import FormUserInformations
+from .register_new_user import RegisterNewUser
 
-__all__ = ["Login", "Home", "SelfInformations", "FormUserInformations"]
+__all__ = ["Login", "Home", "SelfInformations", "FormUserInformations", "RegisterNewUser"]
