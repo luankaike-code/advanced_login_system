8de2ded9268179309c958afc125409f14cd6ce4f
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-03 15:16:50 -0300

    db: Refractore RowUser to no crash with none fields

--- src/als_db/table_managers/types/row_user.py
+++ src/als_db/table_managers/types/row_user.py
@@ -4,9 +4,9 @@ from typing import Dict
 class RowUser:
 	def __init__(self, user_data: Dict[str, RowItemType]):
 		self.dict = user_data
-		self.id = user_data["user_id"]
-		self.name = user_data["name"]
-		self.email = user_data["email"]
-		self.password = user_data["password"]
-		self.tel = user_data["tel"]
-		self.has_sudo_access = user_data["has_sudo_access"]
+		self.id = user_data.get("user_id")
+		self.name = user_data.get("name")
+		self.email = user_data.get("email")
+		self.password = user_data.get("password")
+		self.tel = user_data.get("tel")
+		self.has_sudo_access = user_data.get("has_sudo_access")
