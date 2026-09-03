0f4a0e5807c0e7f2ba76285032e13e8563e572c6
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-03 15:20:11 -0300

    client: Now UpdateUserInformationsPage have usser logged information how default values

--- src/als_client/pages/update_user_informations_page.py
+++ src/als_client/pages/update_user_informations_page.py
@@ -1,5 +1,6 @@
 from .abstract_form_user_informations_page import AbstractFormUserInformationsPage
+from als_requests import Request
 
 class UpdateUserInformationsPage(AbstractFormUserInformationsPage):
 	def __init__(self, master, geometry: str = "400x500"):
-		super().__init__(master, button_text="Atualizar", title="Atualizar usuário", geometry=geometry)
+		super().__init__(master, button_text="Atualizar", title="Atualizar usuário", default_inputs_value=Request.get_self_infos(), geometry=geometry)
