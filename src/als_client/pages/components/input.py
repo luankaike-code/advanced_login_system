5a4f906c1b3fda85f62281a98e6bd676f71bb9b8
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-03 14:48:18 -0300

    client: Create error feedback to Inputs

--- src/als_client/pages/components/input.py
+++ src/als_client/pages/components/input.py
@@ -10,5 +10,15 @@ class Input(Component):
 			"entry": ctk.CTkEntry(window, placeholder_text=placeholder)
 		})
 
+		self.default_entry_border_color = self._widgets["entry"].cget("border_color")
+
+	def set_normal(self) -> None:
+		self._widgets["entry"].configure(border_color=self.default_entry_border_color)
+		self.master.focus_set()
+
+	def set_error(self) -> None:
+		self._widgets["entry"].configure(border_color="#FF5733")
+		self._widgets["entry"].focus_set()
+
 	def get_value(self) -> str:
 		return self._widgets["entry"].get()
